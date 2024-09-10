48dbf34b4245b13ccc81d70d4b473ce9fc40aea3
whfh3832 <whfh3832@naver.com>
2024-09-10 12:49:06 +0900

    0910

--- src/main/java/com/project/oneshot/humanResource/EmployeeMapper.java
+++ src/main/java/com/project/oneshot/humanResource/EmployeeMapper.java
@@ -1,4 +1,20 @@
 package com.project.oneshot.humanResource;
 
+import com.project.oneshot.entity.EmployeeVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface EmployeeMapper {
+
+    // 모든 사원 조회
+    List<EmployeeVO> getAllEmployees();
+
+    // 사원 생성 및 업데이트
+    void insertOrUpdateEmployee(EmployeeVO employeeVo);
+
+    // 사원 삭제
+    void deleteEmployee(Long id);
+
 }
--- src/main/java/com/project/oneshot/humanResource/EmployeeServiceImpl.java
+++ src/main/java/com/project/oneshot/humanResource/EmployeeServiceImpl.java
@@ -1,4 +1,22 @@
 package com.project.oneshot.humanResource;
 
-public class EmployeeServiceImpl {
+import com.project.oneshot.entity.EmployeeVO;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public List<EmployeeVO> getAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public EmployeeVO createOrUpdateEmployee(EmployeeVO employeeVo) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
 }
